a3208bb1b0b9815866e039b525d220c3ef1fe7b5
AdenJowsey <adenajowsey@gmail.com>
2024-09-27 11:51:41 +1200

    Adding two binding and event emitting to user input

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NewInvestmentData } from '../investment-results/investment-result.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<NewInvestmentData>();
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  duration = 0;
 
+  onCalculate() {
+    this.calculate.emit({
+      initialInvestment: this.initialInvestment,
+      annualInvestment: this.annualInvestment,
+      expectedReturn: this.expectedReturn,
+      duration: this.duration,
+    });
+  }
 }
