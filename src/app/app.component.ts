9f6305fdb9e4393d511fe2be7a5d60269c0449d7
AdenJowsey <adenajowsey@gmail.com>
2024-09-27 11:20:06 +1200

    App structure Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
