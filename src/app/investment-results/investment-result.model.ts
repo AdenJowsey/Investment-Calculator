fec8f624a4a70d1334adf3980bb15048c532002b
AdenJowsey <adenajowsey@gmail.com>
2024-09-27 12:20:21 +1200

    Consolidating resources

--- src/app/investment-results/investment-result.model.ts
+++ src/app/investment-results/investment-result.model.ts
@@ -13,3 +13,33 @@ export interface NewInvestmentData {
   expectedReturn: number;
   duration: number;
 }
+
+export function calculateInvestmentResults(
+  investmentInput: NewInvestmentData
+): InvestementResults[] {
+  const annualData = [];
+  let investmentValue = investmentInput.initialInvestment;
+
+  for (let i = 0; i < investmentInput.duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear =
+      investmentValue * (investmentInput.expectedReturn / 100);
+    investmentValue += interestEarnedInYear + investmentInput.annualInvestment;
+    const totalInterest =
+      investmentValue -
+      investmentInput.annualInvestment * year -
+      investmentInput.initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: investmentInput.annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested:
+        investmentInput.initialInvestment +
+        investmentInput.annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
