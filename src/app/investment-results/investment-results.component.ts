23e6938d4f9bb32f9650e1aa7c265523ccbb367a
AdenJowsey <adenajowsey@gmail.com>
2024-09-27 12:20:38 +1200

    adding result display

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  calculateInvestmentResults,
+  NewInvestmentData,
+} from './investment-result.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
+  @Input({ required: true }) currentInvestmentInput?: NewInvestmentData;
 
+  get investmentResults() {
+    return this.currentInvestmentInput
+      ? calculateInvestmentResults(this.currentInvestmentInput)
+      : undefined;
+  }
 }
